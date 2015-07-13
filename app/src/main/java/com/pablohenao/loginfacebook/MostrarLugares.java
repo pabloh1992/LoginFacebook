4a4d0f7e6444b48aadf12d135cec4a75f76873ea
Pabloh <pabloh1992@gmail.com>
2015-07-12 20:16:44 -0500

    Login + Mostrar Lugares

--- app/src/main/java/com/pablohenao/loginfacebook/MostrarLugares.java
+++ app/src/main/java/com/pablohenao/loginfacebook/MostrarLugares.java
@@ -1,39 +1,54 @@
 package com.pablohenao.loginfacebook;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
-public class MostrarLugares extends ActionBarActivity {
+public class MostrarLugares extends Activity {
+
+    ListViewAdapterLugares adapter;
+
+    String[] titulo = new String[]{
+            "Restaurantes",
+            "Bares",
+            "Cafes",
+            "Discotecas",
+    };
+
+    int[] imagenes = {
+            R.drawable.restaurant,
+            R.drawable.bar,
+            R.drawable.coffee,
+            R.drawable.disco
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_lugares);
-    }
 
+        final ListView lista = (ListView) findViewById(R.id.listView1);
+        adapter = new ListViewAdapterLugares(this, titulo, imagenes);
+        lista.setAdapter(adapter);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_mostrar_lugares, menu);
-        return true;
-    }
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "presiono " + i, Toast.LENGTH_SHORT).show();
+            }
+        });
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "presiono LARGO " + i, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
